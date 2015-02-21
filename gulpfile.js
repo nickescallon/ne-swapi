a38d6e9f14948d32d70600176ef8c1975b957eab
Nick Escallon <nickescallon@gmail.com>
2015-02-21 16:01:21 -0500

    adds gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+// dependencies
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
+
+// paths
+var appName = 'ne-swapi',
+    source = ['./lib/**/*.js'],
+    destination = './dist/';
+
+gulp.task('js', function() {
+  gulp.src(source)
+  .pipe(concat(appName + '.js'))
+  .pipe(gulp.dest(destination))
+  .pipe(ngAnnotate())
+  .pipe(uglify())
+  .pipe(rename(appName + '.min.js'))
+  .pipe(gulp.dest(destination))
+});
+
+gulp.task('default', ['js']);
